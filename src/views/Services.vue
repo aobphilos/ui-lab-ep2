6e6ad9a7eea988d9ee83883e07e03a92ba0092d9
Boonyarit Wiriyagulpat <boonyarit@ekoapp.com>
2020-07-27 17:46:41 +0700

    feat(service): update text in Service page

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <loader-fade-out/>
+    <loader-fade-out />
     <!-- Services heading-->
     <section id="services_header" class="section_header">
       <div class="container">
@@ -20,87 +20,56 @@
           <div class="col-md-7 col-md-offset-0 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
             <div class="row padding-tb50">
               <div class="col-md-11">
-                <h4 class="h4-type1">{{ $t('content.services.section.identification.subject')}}</h4>
-                <div>
-                  <div class="p-type-3 color-grey margin-t20">
-                    <ol>
-                      <li>{{ $t('content.services.section.identification.lines[0]')}}</li>
-                      <li>{{ $t('content.services.section.identification.lines[1]')}}</li>
-                      <li>{{ $t('content.services.section.identification.lines[2]')}}</li>
-                      <li>{{ $t('content.services.section.identification.lines[3]')}}</li>
-                    </ol>
-                  </div>
-                  <p
-                    class="p-type-3 color-grey margin-t20"
-                  >{{ $t('content.services.section.identification.example.subject')}}</p>
-                  <div class="example">
-                    <p
-                      class="p-type-3 color-grey margin-t20"
-                    >{{ $t('content.services.section.identification.example.lines[0]')}}</p>
-                    <p
-                      class="p-type-3 color-grey margin-t20"
-                    >{{ $t('content.services.section.identification.example.lines[1]')}}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="row padding-tb50">
-              <div class="col-md-11">
-                <h4 class="h4-type1">{{ $t('content.services.section.verbal.subject')}}</h4>
-                <div>
-                  <div class="p-type-3 color-grey margin-t20">
-                    <ol>
-                      <li>{{ $t('content.services.section.verbal.lines[0]')}}</li>
-                    </ol>
-                  </div>
-                </div>
+                <h4 class="h4-type1">
+                  {{ $t('content.services.section.notice.lines[0]') }}
+                </h4>
               </div>
             </div>
           </div>
           <div class="col-md-5 col-xs-12 imgage-box">
             <div class="row padding-tb25">
               <a href="/img/services/full/service-1.jpg" data-lightbox="service-lightbox">
-                <img src="/img/services/service-1.jpg" alt="Services">
+                <img src="/img/services/service-1.jpg" alt="Services" />
               </a>
             </div>
             <div class="row padding-tb25">
               <a href="/img/services/full/service-2.jpg" data-lightbox="service-lightbox">
-                <img src="/img/services/service-2.jpg" alt="Services">
+                <img src="/img/services/service-2.jpg" alt="Services" />
               </a>
             </div>
             <div class="row padding-tb25">
               <a href="/img/services/full/service-3.jpg" data-lightbox="service-lightbox">
-                <img src="/img/services/service-3.jpg" alt="Services">
+                <img src="/img/services/service-3.jpg" alt="Services" />
               </a>
             </div>
             <div class="row padding-tb25">
               <a href="/img/services/full/service-4.jpg" data-lightbox="service-lightbox">
-                <img src="/img/services/service-4.jpg" alt="Services">
+                <img src="/img/services/service-4.jpg" alt="Services" />
               </a>
             </div>
             <div class="row padding-tb25">
               <a href="/img/services/full/service-5.jpg" data-lightbox="service-lightbox">
-                <img src="/img/services/service-5.jpg" alt="Services">
+                <img src="/img/services/service-5.jpg" alt="Services" />
               </a>
             </div>
             <div class="row padding-tb25">
               <a href="/img/services/full/service-6.jpg" data-lightbox="service-lightbox">
-                <img src="/img/services/service-6.jpg" alt="Services">
+                <img src="/img/services/service-6.jpg" alt="Services" />
               </a>
             </div>
             <div class="row padding-tb25">
               <a href="/img/services/full/service-7.jpg" data-lightbox="service-lightbox">
-                <img src="/img/services/service-7.jpg" alt="Services">
+                <img src="/img/services/service-7.jpg" alt="Services" />
               </a>
             </div>
             <div class="row padding-tb25">
               <a href="/img/services/full/service-8.jpg" data-lightbox="service-lightbox">
-                <img src="/img/services/service-8.jpg" alt="Services">
+                <img src="/img/services/service-8.jpg" alt="Services" />
               </a>
             </div>
             <div class="row padding-tb25">
               <a href="/img/services/full/service-9.jpg" data-lightbox="service-lightbox">
-                <img src="/img/services/service-9.jpg" alt="Services">
+                <img src="/img/services/service-9.jpg" alt="Services" />
               </a>
             </div>
           </div>
@@ -128,22 +97,28 @@ export default class PageServices extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-  #services {
-    .imgage-box {
-      .padding-tb25 {
-        padding: 15px 0;
-        margin: 10px 0;
-        text-align: center;
-        border: 2px solid lightgray;
-        border-radius: 25px;
-      }
-    }
-    ol {
-      list-style: decimal;
-      padding: 0 0 0 35px;
-    }
-    .example {
-      padding: 0 0 0 20px;
+#services {
+  .imgage-box {
+    .padding-tb25 {
+      padding: 15px 0;
+      margin: 10px 0;
+      text-align: center;
+      border: 2px solid lightgray;
+      border-radius: 25px;
     }
   }
+  ol {
+    list-style: decimal;
+    padding: 0 0 0 35px;
+  }
+  .example {
+    padding: 0 0 0 20px;
+  }
+  .notice {
+    font-style: normal;
+    color: red;
+    font-weight: 600;
+    font-size: 15px;
+  }
+}
 </style>
